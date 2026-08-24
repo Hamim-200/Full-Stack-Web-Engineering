d93dc97a379d69bd7c7c4c8d6b798a2924875d44
Hamim Ahmed <hamim.ahmed541@gmail.com>
2026-08-25 00:22:52 +0600

    module 24 done

---
+++ Module-24/src/4.calculateCartTotal.ts
@@ -0,0 +1,26 @@
+type Product = {
+    name: string;
+    price: number;
+}
+
+const calculateCartTotal = (products: Product[]): number => {
+    const total: number = products.reduce((sum: number, item: Product) => sum + item.price, 0);
+    return total;
+}
+
+
+const products = [
+    { name: "Keyboard", price: 1500 },
+    { name: "Mouse", price: 800 },
+    { name: "USB Cable", price: 300 }
+];
+
+
+console.log(calculateCartTotal(products)); // Output: 2600
+
+const products2 = [ 
+{ name: "Book", price: 500 }, 
+{ name: "Pen", price: 50 }, 
+{ name: "Bag", price: 1200 } 
+]; 
+console.log(calculateCartTotal(products2)); // Output: 1750
---
+++ Module-24/src/5.getStudentResult.ts
@@ -0,0 +1,36 @@
+interface Student {
+    name: string;
+    marks: number[]
+}
+
+interface Result {
+    name: string
+    average: number
+    result: string
+}
+
+const getStudentResult = (student: Student): Result => {
+    const marks: number[] = student.marks
+    const totalMarks: number = marks.reduce((sum: number, mark: number) => sum + mark, 0)
+    const average: number = totalMarks / marks.length
+    const result: string = average >= 40 ? "Pass" : "Fail"
+    return {
+        name: student.name,
+        average: average,
+        result: result
+    }
+}
+
+console.log(getStudentResult({
+    name: "Nabil",
+    marks: [30, 35, 40, 25]
+}
+)
+)
+
+
+
+console.log(getStudentResult({
+    name: "Rafi",
+    marks: [80, 75, 90, 85]
+}));
